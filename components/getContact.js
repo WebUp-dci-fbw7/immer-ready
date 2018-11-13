5f2d662c451d350f12d0a8d13ef5651c1e295b56
ghost-fa <fayyad_r22@yahoo.com>
2018-11-13 21:04:35 +0100

    create componente getContact and reander the icons

---
+++ components/getContact.js
@@ -0,0 +1,18 @@
+import React, {Component, Fragment} from 'react'
+import   {View,TextInput} from "react-native";
+import { Ionicons, Octicons,AntDesign } from '@expo/vector-icons';
+
+
+export default class GetContact extends Component{
+  render(){
+    return (
+      <View>
+      <Octicons name="circledowno" size={250} color="green"  style={{marginLeft: 120}}/>
+
+
+
+   <Octicons name="downcircleo" size={250} color="green"   style={{marginLeft: 120}}/>
+</View>
+    )
+  }
+}
