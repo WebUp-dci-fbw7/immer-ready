4962f12c46f9bb1647d6b19f67d09fdae506c3b7
ghost-fa <fayyad_r22@yahoo.com>
2018-11-13 22:44:36 +0100

    add TeaxtInput and style and add new icons

--- components/getContact.js
+++ components/getContact.js
@@ -1,18 +1,27 @@
 import React, {Component, Fragment} from 'react'
-import   {View,TextInput} from "react-native";
+import   {View,TextInput, Text} from "react-native";
 import { Ionicons, Octicons,AntDesign, FontAwesome } from '@expo/vector-icons';
 
 
 export default class GetContact extends Component{
   render(){
     return (
+          <View>
       <View>
-      <FontAwesome name="chevron-circle-up" size={260} color="green"  style={{marginLeft: 93}}/>
+      <FontAwesome name="caret-up" size={300}  style={{marginLeft: 93, marginBottom:0}}/>
+      </View>
+
+      <TextInput
+               style={{marginLeft: 93, marginBottom:0}}
+               placeholder="Type here to translate!"
+               readOnly
+             />
 
 
-
-   <FontAwesome name="chevron-circle-down" size={260} color="green"   style={{marginLeft: 93}}/>
+  <View>
+   <FontAwesome name="caret-down" size={300}    style={{marginLeft: 93, marginTop:0}}/>
 </View>
+  </View>
     )
   }
 }
