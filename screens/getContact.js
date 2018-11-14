8f9f2d0481e84b7cb86e1ddd9cb0e1467658b8e5
ghost-fa <fayyad_r22@yahoo.com>
2018-11-14 16:13:27 +0100

    add icons in getContact

--- screens/getContact.js
+++ screens/getContact.js
@@ -6,7 +6,22 @@ export default class GetContact extends Component{
 
 render(){
   return(
+    <View style={{marginLeft:'23%'}}>
+        <View>
+        <FontAwesome name="caret-up" size={350}/>
+        </View>
+  <View style={{marginLeft:'15%'}}>
+        <TextInput
 
+                 placeholder="Immer Ready!"
+
+               />
+  </View>
+
+    <View>
+     <FontAwesome name="caret-down" size={350}/>
+  </View>
+    </View>
   )
 }
 
