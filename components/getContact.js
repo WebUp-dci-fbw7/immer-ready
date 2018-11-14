492b46239039323bddd4fff7747aee1a06bc002e
ghost-fa <fayyad_r22@yahoo.com>
2018-11-14 09:59:45 +0100

    change the style

--- components/getContact.js
+++ components/getContact.js
@@ -5,21 +5,22 @@ import { Ionicons, Octicons,AntDesign, FontAwesome } from '@expo/vector-icons';
 
 export default class GetContact extends Component{
   render(){
+
     return (
           <View>
       <View>
-      <FontAwesome name="caret-up" size={300}  style={{marginLeft: 93, marginBottom:0}}/>
+      <FontAwesome name="caret-up" size={300}  style={{marginLeft: 94, marginBottom:0}}/>
       </View>
 
       <TextInput
-               style={{marginLeft: 93, marginBottom:0}}
-               placeholder="Type here to translate!"
-               editable={{false}}
+               style={{marginLeft: 100, marginBottom:0}}
+               placeholder="Immer Ready!"
+
              />
 
 
   <View>
-   <FontAwesome name="caret-down" size={300}    style={{marginLeft: 93, marginTop:0}}/>
+   <FontAwesome name="caret-down" size={300}    style={{marginLeft: 94, marginTop:0}}/>
 </View>
   </View>
     )
