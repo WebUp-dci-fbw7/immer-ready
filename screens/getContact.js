dbea56df3cb833d55e4f769160445d7d8d987d0d
ghost-fa <fayyad_r22@yahoo.com>
2018-11-14 15:59:59 +0100

    change component linksScreen to get contact and import the icons

---
+++ screens/getContact.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import   {View,TextInput, Text} from "react-native";
+import { Ionicons, Octicons,AntDesign, FontAwesome } from '@expo/vector-icons';
+
+export default class GetContact extends Component{
+
+render(){
+  return(
+
+  )
+}
+
+
+}
