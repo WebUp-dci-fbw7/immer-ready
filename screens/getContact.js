f17d3d885bbff20640e756cc6df0297dd874e434
Majed Shrendan <majedshrndan45@gmail.com>
2018-11-15 11:49:17 +0100

    fix some styles

--- screens/getContact.js
+++ screens/getContact.js
@@ -1,40 +1,32 @@
-import React, {Component} from 'react'
-import   {View,TextInput, Text, StyleSheet} from "react-native";
-import { Ionicons, Octicons,AntDesign, FontAwesome } from '@expo/vector-icons';
+import React, { Component } from "react";
+import { View, TextInput, Text, StyleSheet } from "react-native";
+import { Ionicons, Octicons, AntDesign, FontAwesome } from "@expo/vector-icons";
 
-export default class GetContact extends Component{
-
-render(){
-  return(
-    <View style={styles.container}>
+export default class GetContact extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
         <View>
-        <FontAwesome name="caret-up" size={350}/>
+          <FontAwesome name="caret-up" size={350} />
         </View>
-  <View style={{marginLeft:'15%'}}>
-        <TextInput
+        <View style={{ marginLeft: "15%" }}>
+          <TextInput placeholder="Immer Ready!" editable={false} />
+        </View>
 
-                 placeholder="Immer Ready!"
-
-               />
-  </View>
-
-    <View>
-     <FontAwesome name="caret-down" size={350}/>
-  </View>
-    </View>
-  )
+        <View>
+          <FontAwesome name="caret-down" size={350} />
+        </View>
+      </View>
+    );
+  }
 }
-
-
-}
-
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    paddingTop:24,
-    alignItems :'stretch',
+    paddingTop: 24,
+    alignItems: "stretch",
 
-    justifyContent: 'center'
-  },
+    justifyContent: "center"
+  }
 });
