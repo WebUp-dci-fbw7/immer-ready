b85a8c92bdddcff2eeeed30df866d9754164138f
ghost-fa <fayyad_r22@yahoo.com>
2018-11-15 10:43:54 +0100

    add the Home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,48 +3,85 @@ import {
   ScrollView,
   View,
   Alert,
-  Navigator
+  StyleSheet,
+  Text,
+
 } from "react-native";
-
+import {Button } from 'react-native-elements';
 import {WebBrowser} from "expo";
-
+import {Ionicons, Feather, Entypo} from '@expo/vector-icons';
 import GetContact from './getContact';
-import Main from './Main';
+import {createStackNavigator} from 'react-navigation';
 
-export default class HomeScreen extends React.Component {
+export default class Main extends React.Component {
   static navigationOptions = {
     header: null
   };
 
 
+
   render() {
+      const { navigate } = this.props.navigation;
     return (
-      <Navigator
-      initialRoute={{
-        id:'Main'
-      }}
-      renderScene={
-        this.navigatorRenderScene
-      }
-      />
+      <View style={styles.container}>
+        <ScrollView>
+          <View style={{
+            height: 100,
+            backgroundColor:'powderblue',
+            justifyContent: 'center'
+          }}>
+            <Ionicons
+              name='md-contacts'
+              size={85}
+              onPress={() => {
+
+                navigate('Secound')}}
+              />
+          </View>
+          <View style={{
+            height: 100,
+            backgroundColor:'skyblue',
+            justifyContent: 'center'
+          }}>
+            <Entypo
+              name='location'
+              size={85}
+              onPress={()=> {
+                Alert.alert('Send Location!')
+              }}
+              />
+          </View>
+
+          <View style={{
+            height: 100,
+            backgroundColor:'steelblue',
+            justifyContent: 'center'
+          }}>
+            <Feather
+              name='phone-call'
+              size= {85}
+
+              onPress={()=> {
+                Alert.alert('Call Contact!')
+              }}
+              />
+          </View>
+    
+
+        </ScrollView>
+      </View>
     );
   }
-
-navigatorRenderScene(route, navigator){
-  _navigator = navigator;
-  switch (route.id){
-    case 'Main':
-    return(<Main navigator={navigator} title='Main' />);
-    case 'GetContact':
-    return(<GetContact navigator={navigator} title='GetContact' />);
-  }
 }
 
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync("https://docs.expo.io/versions/latest/guides/development-mode");
-  };
 
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync("https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes");
-  };
-}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    paddingTop:24,
+    alignItems :'stretch',
+
+    justifyContent: 'center'
+  },
+});
