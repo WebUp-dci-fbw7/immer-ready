cf65777fd0bc2f6cc4d0e4754132a4f46578f6f9
mihneavlad <mihnel@yahoo.com>
2018-11-19 11:31:22 +0100

    Managed to get the initial text ("Waiting for location") on the main screen and then to update it based on the obtained location.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,17 +9,16 @@ import {
   View
 } from "react-native";
 import { WebBrowser } from "expo";
-import { Button } from 'react-native-elements';
-import { Constants, Location, Permissions, Contacts } from 'expo';
+import { Button } from "react-native-elements";
+import { Constants, Location, Permissions, Contacts } from "expo";
 
 import { MonoText } from "../components/StyledText";
 
 export default class HomeScreen extends React.Component {
-
   state = {
     latitude: null,
     longitude: null,
-    errorMessage: null,
+    errorMessage: null
   };
 
   static navigationOptions = {
@@ -27,26 +26,26 @@ export default class HomeScreen extends React.Component {
   };
 
   _getLocationAsync = async () => {
-  let { status } = await Permissions.askAsync(Permissions.LOCATION);
-  if (status !== 'granted') {
-    this.setState({
-      errorMessage: 'Permission to access location was denied',
-    });
-  }
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") {
+      this.setState({
+        errorMessage: "Permission to access location was denied"
+      });
+    }
 
-  let location = await Location.getCurrentPositionAsync({});
-  this.setState({ latitude: location.coords.latitude,
-                  longitude: location.coords.longitude });
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude
+    });
   };
 
-
   render() {
-
-    let latitude = '';
-    let longitude = '';
+    let latitude = "";
+    let longitude = "";
+    let initialText = "Waiting for location";
     if (this.state.errorMessage) {
       text = this.state.errorMessage;
-
     } else if (this.state.latitude && this.state.longitude) {
       latitude = JSON.stringify(this.state.latitude);
       longitude = JSON.stringify(this.state.longitude);
@@ -54,49 +53,59 @@ export default class HomeScreen extends React.Component {
     }
 
     async function alowSMS() {
+      const permission = await Permissions.askAsync(
+        Permissions.CONTACTS,
+        Permissions.SMS
+      );
 
-      const permission = await Permissions.askAsync(Permissions.CONTACTS, Permissions.SMS);
-
-      if (permission.status !== 'granted') {
+      if (permission.status !== "granted") {
         // Permission was denied...
         return;
       }
-      const { result } = await Expo.SMS.sendSMSAsync('+4915163180836', `https://www.google.com/maps/@${latitude},${longitude},17z`);
+      const { result } = await Expo.SMS.sendSMSAsync(
+        "+4915163180836",
+        `https://www.google.com/maps/@${latitude},${longitude},17z`
+      );
     }
 
     return (
       <View style={styles.container}>
-        <Text style={styles.paragraph}>https://www.google.com/maps/@{latitude},{longitude},17z</Text>
+        <Text style={styles.paragraph}>
+          {`${latitude}` && `${longitude}`
+            ? `https://www.google.com/maps/@${latitude},${longitude},17z`
+            : "Waiting for location..."}
+        </Text>
+
         <View>
-          <Button title='Get location' onPress={this._getLocationAsync} />
-          <Button title='Send SMS' onPress={alowSMS} />
+          <Button title="Get location" onPress={this._getLocationAsync} />
+          <Button title="Send location via SMS" onPress={alowSMS} />
         </View>
       </View>
     );
   }
 
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use
-          useful development tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
+  // _maybeRenderDevelopmentModeWarning() {
+  //   if (__DEV__) {
+  //     const learnMoreButton = (
+  //       <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
+  //         Learn more
+  //       </Text>
+  //     );
+  //
+  //     return (
+  //       <Text style={styles.developmentModeText}>
+  //         Development mode is enabled, your app will be slower but you can use
+  //         useful development tools. {learnMoreButton}
+  //       </Text>
+  //     );
+  //   } else {
+  //     return (
+  //       <Text style={styles.developmentModeText}>
+  //         You are not in development mode, your app will run at full speed.
+  //       </Text>
+  //     );
+  //   }
+  // }
 
   _handleLearnMorePress = () => {
     WebBrowser.openBrowserAsync(
@@ -114,103 +123,14 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: "#ecf0f1"
   },
   paragraph: {
     margin: 24,
     fontSize: 18,
-    textAlign: 'center',
-  },
+    textAlign: "center"
+  }
 });
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff"
-//   },
-//   developmentModeText: {
-//     marginBottom: 20,
-//     color: "rgba(0,0,0,0.4)",
-//     fontSize: 14,
-//     lineHeight: 19,
-//     textAlign: "center"
-//   },
-//   contentContainer: {
-//     paddingTop: 30
-//   },
-//   welcomeContainer: {
-//     alignItems: "center",
-//     marginTop: 10,
-//     marginBottom: 20
-//   },
-//   welcomeImage: {
-//     width: 100,
-//     height: 80,
-//     resizeMode: "contain",
-//     marginTop: 3,
-//     marginLeft: -10
-//   },
-//   getStartedContainer: {
-//     alignItems: "center",
-//     marginHorizontal: 50
-//   },
-//   homeScreenFilename: {
-//     marginVertical: 7
-//   },
-//   codeHighlightText: {
-//     color: "rgba(96,100,109, 0.8)"
-//   },
-//   codeHighlightContainer: {
-//     backgroundColor: "rgba(0,0,0,0.05)",
-//     borderRadius: 3,
-//     paddingHorizontal: 4
-//   },
-//   getStartedText: {
-//     fontSize: 17,
-//     color: "rgba(96,100,109, 1)",
-//     lineHeight: 24,
-//     textAlign: "center"
-//   },
-//   tabBarInfoContainer: {
-//     position: "absolute",
-//     bottom: 0,
-//     left: 0,
-//     right: 0,
-//     ...Platform.select({
-//       ios: {
-//         shadowColor: "black",
-//         shadowOffset: { height: -3 },
-//         shadowOpacity: 0.1,
-//         shadowRadius: 3
-//       },
-//       android: {
-//         elevation: 20
-//       }
-//     }),
-//     alignItems: "center",
-//     backgroundColor: "#fbfbfb",
-//     paddingVertical: 20
-//   },
-//   tabBarInfoText: {
-//     fontSize: 17,
-//     color: "rgba(96,100,109, 1)",
-//     textAlign: "center"
-//   },
-//   navigationFilename: {
-//     marginTop: 5
-//   },
-//   helpContainer: {
-//     marginTop: 15,
-//     alignItems: "center"
-//   },
-//   helpLink: {
-//     paddingVertical: 15
-//   },
-//   helpLinkText: {
-//     fontSize: 14,
-//     color: "#2e78b7"
-//   }
-// });
