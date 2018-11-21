f90d823d744afc933a9fc9b2c515eabb29c1fdaf
Saadat1234 <ahmadkhan.saadat@gmail.com>
2018-11-21 16:55:22 +0100

    styled home

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,21 +15,21 @@ export default class HomeScreen extends React.Component {
     return (<View style={styles.container}>
 
       <View style={styles.contacts}>
-        <Ionicons style={{
+        <Ionicons name='md-contacts' style={{
             marginLeft: responsiveWidth(7),
             marginBottom: responsiveHeight(35),
             fontSize: RF(22)
-          }} name='md-contacts' onPress={() => {
+          }} onPress={() => {
             Alert.alert('you pressed to get contacts')
           }}/>
 
-      {/* </View>
+      </View>
       <View style={styles.location}>
         <Entypo style={{
             marginLeft: responsiveWidth(7),
             marginBottom: responsiveHeight(35),
             fontSize: RF(19),
-            color: "blue",
+            color: "blue"
           }} name='location' onPress={() => {
             Alert.alert('Send Location!')
           }}/>
@@ -39,10 +39,7 @@ export default class HomeScreen extends React.Component {
             marginLeft: responsiveWidth(-15),
             fontSize: RF(17),
             color: "green",
-            position: 'absolute',
-            // textAlign: "center",
-            // justifyContent:'center',
-            // marginTop: responsiveHeight(20)
+            position: 'absolute'
           }} name='phone-call' onPress={() => {
             Alert.alert('Call Contact!')
           }}/>
@@ -64,9 +61,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'row',
-    // paddingTop: responsiveHeight(4),
     backgroundColor: 'yellow',
-    // alignItems: 'center',
     justifyContent: 'center',
     position: 'absolute',
     top: 0,
@@ -85,8 +80,7 @@ const styles = StyleSheet.create({
     borderRightWidth: responsiveWidth(0.2),
     borderLeftWidth: responsiveWidth(0.2),
     borderLeftColor: 'white',
-    borderRightColor: 'white',
-
+    borderRightColor: 'white'
   },
 
   location: {
@@ -97,30 +91,23 @@ const styles = StyleSheet.create({
     borderRightWidth: responsiveWidth(0.2),
     borderLeftWidth: responsiveWidth(0.2),
     borderLeftColor: 'white',
-    borderRightColor: 'white',
+    borderRightColor: 'white'
   },
 
   phone: {
-    // backgroundColor: 'transparent',
     borderStyle: 'solid',
     borderLeftWidth: responsiveWidth(50),
-    borderRightWidth:responsiveWidth(50),
+    borderRightWidth: responsiveWidth(50),
     borderBottomWidth: responsiveWidth(80),
     borderBottomColor: '#bdbdbd',
     borderLeftColor: '#607d8b',
     borderRightColor: '#555',
-    // width: responsiveWidth(0),
-    // height: responsiveHeight(0),
     flex: 1,
-    // width: responsiveWidth(100),
-    // height: responsiveHeight(40),
-    // backgroundColor: '#f00',
     overflow: 'visible',
     zIndex: 5,
     position: 'absolute',
     bottom: 0,
-    justifyContent: 'center',
-
+    justifyContent: 'center'
   }
 
-}); */}
+});
