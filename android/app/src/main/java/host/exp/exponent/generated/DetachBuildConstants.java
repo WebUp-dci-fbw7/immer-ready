3141596a326448ce637372e3b9b7e5836b669f0c
Majed Shrendan <majedshrndan45@gmail.com>
2018-12-13 13:43:23 +0100

    eject the app and set the android folder

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expaf64eb98088c49bf80de33d37c23712e://ps-9ny.immerready.immer-ready.exp.direct:80";
+
+}
